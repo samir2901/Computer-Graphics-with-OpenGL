ccb3353275df50a63853427ffb2d89b6331d1455
samir2901 <thunder.cse2018@gmail.com>
2020-08-11 11:30:59 +0530

    Bresenham Algorithm Added'

---
+++ src/Bresenham_Line.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<GLFW/glfw3.h>
+#include<gl/GL.h>
+
+
+//only for m < 1 ie slope < 1
+void BresenhamLine(int x1, int y1, int x2, int y2) {
+	int x, y, dx, dy, p;
+	x = x1; 
+	y = y1;
+	dx = x2 - x1;
+	dy = y2 - y1;
+	float m = dy / (float)dx;
+	p = 2 * dx - dy;	
+	glBegin(GL_POINTS);
+	while (x <= x2) {	
+		glVertex2i(x, y);
+		x++;
+		if (p < 0) {
+			p = p + 2 * dy;
+		}
+		else {
+			p = p + 2 * (dy - dx);
+			y++;
+		}
+		glVertex2i(x, y);
+	}
+	glEnd();
+	glFlush();
+}
+
