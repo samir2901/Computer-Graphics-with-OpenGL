15b541daf5ae7a2cedab1d23afa54a6081acc934
samir2901 <thunder.cse2018@gmail.com>
2020-08-20 10:59:06 +0530

    Update Header File

--- HeaderFiles/drawgraphics.h
+++ HeaderFiles/drawgraphics.h
@@ -1,4 +1,5 @@
 #pragma once
 
-void DDA(float, float, float, float);
-void BresenhamLine(int, int, int, int);
+void DDA(float startX, float startY, float endX, float endY);
+void BresenhamLine(int startX, int startY, int endX, int endY);
+void midPointCircle(int xCenter, int yCenter, int radius);
